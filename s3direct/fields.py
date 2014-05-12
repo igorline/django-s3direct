51f0e9dac036ffa519a0cc44c91fe93e1d43e6f0
Igor Line <linesethenoiz@gmail.com>
2014-05-12 10:11:25 +0200

    generate directory out of upload path

--- s3direct/fields.py
+++ s3direct/fields.py
@@ -1,13 +1,22 @@
+import os
+import datetime
+
 from django.db.models import Field
 from django.forms import widgets
 from s3direct.widgets import S3DirectEditor
 from django.conf import settings
+from django.utils.encoding import force_str, force_text
 
+import sys
 
 class S3DirectField(Field):
     def __init__(self, *args, **kwargs):
         upload_to = kwargs.pop('upload_to', '')
-        self.widget = S3DirectEditor(upload_to=upload_to)
+        self.upload_to = upload_to
+        if callable(upload_to):
+            self.generate_filename = upload_to
+
+        self.widget = S3DirectEditor(upload_to=self.get_directory_name())
         super(S3DirectField, self).__init__(*args, **kwargs)
 
     def get_internal_type(self):
@@ -17,6 +26,12 @@ class S3DirectField(Field):
         defaults = {'widget': self.widget}
         defaults.update(kwargs)
         return super(S3DirectField, self).formfield(**defaults)
+
+    def get_directory_name(self):
+        return os.path.normpath(force_text(datetime.datetime.now().strftime(force_str(self.upload_to))))
+
+    def generate_filename(self, instance, filename):
+        return os.path.join(self.get_directory_name(), self.get_filename(filename))
 
 
 if 'south' in settings.INSTALLED_APPS:
